45fa798f6df3a8c452d8f911452027a6c6f74160
Kiyoshi <kt.kiyoshi@gmail.com>
2013-09-10 03:21:27 -0700

    Add start_at, end_at

---
+++ db/migrate/20130721044320_create_cookings.rb
@@ -0,0 +1,20 @@
+class CreateCookings < ActiveRecord::Migration
+  def change
+    create_table :cookings do |t|
+      t.string :cooking_name
+      t.integer :user_id
+      t.datetime :start_at
+      t.datetime :end_at
+      t.integer :meal_type
+      t.float :assesment
+      t.text :my_comment
+      t.text :image1
+      t.text :image2
+      t.text :image3
+      t.integer :category_id
+      t.boolean :display
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130830133141) do
+ActiveRecord::Schema.define(:version => 20130831063339) do
 
   create_table "cookings", :force => true do |t|
     t.string   "cooking_name"
     t.integer  "user_id"
-    t.datetime "meal_date"
+    t.datetime "start_at"
+    t.datetime "end_at"
     t.integer  "meal_type"
     t.float    "assesment"
     t.text     "my_comment"
@@ -32,11 +33,21 @@ ActiveRecord::Schema.define(:version => 20130830133141) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.integer  "uid"
-    t.string   "provider"
-    t.string   "access_token"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
